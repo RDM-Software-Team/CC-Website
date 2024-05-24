c56afe5692b013aba199ae57f1d192e71140aa9d
MthoKhumalo <7mtho.khumalo@gmail.com>
2024-05-24 21:19:08 +0200

    revert

--- database/DBConn.inc.php
+++ database/DBConn.inc.php
@@ -1,13 +1,11 @@
 <?php
-    $dhost = "sql309.infinityfree.com";
-    $dname ="if0_36608597_computer_complexdb";
-    $dbusername = "if0_36608597";
-    $dbpassword = "ComputerComplex";
+
+    $dsn = "mysql:host=localhost;dname:computer_complex";
+    $dbusername = "root";
+    $dbpassword = "cS4FJ?";
 
     try{
-        $dsn = "mysql:host=$dhost;dbname=$dname";
         $pdo = new PDO($dsn, $dbusername, $dbpassword);                 //connection to database
-        echo "Connected :) ";
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  //error handling
     }catch(PDOException $e) {
         echo "Connection failed: ". $e->getMessage();                   //error output
